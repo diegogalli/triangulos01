a60c8d2f23fd5300bc7991b3d127e53272d78e5e
diegogalli <84423207+diegogalli@users.noreply.github.com>
2021-07-26 14:52:43 -0300

    Triangulos

---
+++ Triangulo2/Triangulo2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triangulo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lado1;
+            int lado2;
+            int lado3;
+           
+            Console.WriteLine("**********************");
+            Console.WriteLine("Validação de Triangulo");
+            Console.WriteLine("**********************");
+
+            Console.WriteLine("Digite o primeiro Lado: ");
+            lado1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo Lado: ");
+            lado2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite o terceiro Lado: ");
+            lado3 = Convert.ToInt32(Console.ReadLine());
+
+            string textoExibicao = string.Format("Lado1: {0} - Lado2: {1} - Lado1: {2} ", lado1, lado2, lado3);
+            Console.WriteLine(textoExibicao);
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Triangulos/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triangulos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Classificação de um Triangulo
+            // 3 Lados iguais = Triangulo Equilatero
+            // 2 Lados iguais e 1 Diferente = Triangulo Isoceles
+            // 3 Lados Diferentes = Triangulo Escaleno
+           
+            int L1;
+            int L2;
+            int L3;
+            string texto;
+
+            Console.WriteLine("Triangulos");
+            Console.WriteLine("Digite L1");
+            texto = Console.ReadLine();
+            L1 = Convert.ToInt32(texto);
+
+            Console.WriteLine("Digite L2");
+            texto = Console.ReadLine();
+            L2 = Convert.ToInt32(texto);
+
+            Console.WriteLine("Digite L3");
+            texto = Console.ReadLine();
+            L3 = Convert.ToInt32(texto);
+
+            Console.WriteLine("Lado 1: " + L1 + " Lado 2: " + L2 + " Lado 3: " + L3);
+            Console.ReadKey();            
+           
+        }
+    }
+}
